6fe4766be74e649256b7ecd7734d34e593ab14a8
Manik Prabhu <inmail.manik@gmail.com>
2026-08-01 22:14:17 +0530

    Add files via upload

---
+++ Task_2.py
@@ -0,0 +1,31 @@
+#Day-2 as an Intern
+
+#Variables
+name = 'Codomax'
+age = 35
+
+print(name + ' ' + str(age))
+
+#Data_types
+print(type(name))
+print(type(age))
+
+#Operators
+b = 10
+c = 4
+print(b + c, b - c, b * c, b / c, b % c, b ** c)
+
+#loops
+
+for i in range (0, 5):
+    print(i)
+
+#functions
+def even():
+    if x % 2 == 0:
+        print("Even number")
+    else:
+        print("Odd number")
+
+x = int(input("Enter a number: "))
+even()
